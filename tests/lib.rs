4c2b624147be3b6a60d36a0d185798d0752086a7
Dominic van Berkel <dominic@baudvine.net>
2016-04-16 19:12:02 +0200

    Tests on the actual content.

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,7 +1,35 @@
+extern crate chrono;
+
 extern crate timexif;
 
+use std::collections::HashMap;
+
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
+
+/// There are four images, two of which have valid timestamps.
 #[test]
-fn do_the_thing() {
+fn correct_number_of_images() {
     let imgs = timexif::sequencer::get_images("tests/sample-imgs");
     assert_eq!(imgs.len(), 2);
+}
+
+#[test]
+fn correct_timestamps() {
+    let dt = |y, m, d, h, min, s|
+    NaiveDateTime::new(NaiveDate::from_ymd(y, m, d), NaiveTime::from_hms(h, min, s));
+
+    let mut expected = HashMap::new();
+
+    expected.insert("tests/sample-imgs/large from dslr.JPG",
+                    dt(2014, 7, 12, 11, 24, 56));
+    expected.insert("tests/sample-imgs/phone photo.jpg",
+                    dt(2016, 4, 16, 12, 27, 14));
+
+    let imgs = timexif::sequencer::get_images("tests/sample-imgs");
+    for img in imgs {
+        let path = img.path.to_str().unwrap();
+        assert!(expected.contains_key(path));
+        let dt = expected.get(path).unwrap();
+        assert_eq!(dt, &img.timestamp);
+    }
 }
