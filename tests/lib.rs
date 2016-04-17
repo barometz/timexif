bc91e9c4ebafecc46fa00ad20def5a1f8355891c
Dominic van Berkel <dominic@baudvine.net>
2016-04-17 10:08:53 +0200

    Additional test images for the next thing: filters

--- tests/lib.rs
+++ tests/lib.rs
@@ -6,10 +6,13 @@ use std::collections::HashMap;
 
 use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
 
+const SIMPLE: &'static str = "tests/sample-imgs/simple/";
+const BRIGHTNESS: &'static str = "tests/sample-imgs/brightness";
+
 /// There are four images, two of which have valid timestamps.
 #[test]
 fn correct_number_of_images() {
-    let imgs = timexif::sequencer::get_images("tests/sample-imgs");
+    let imgs = timexif::sequencer::get_images(SIMPLE);
     assert_eq!(imgs.len(), 2);
 }
 
@@ -20,12 +23,12 @@ fn correct_timestamps() {
 
     let mut expected = HashMap::new();
 
-    expected.insert("tests/sample-imgs/large from dslr.JPG",
+    expected.insert(format!("{}{}", SIMPLE, "large from dslr.JPG"),
                     dt(2014, 7, 12, 11, 24, 56));
-    expected.insert("tests/sample-imgs/phone photo.jpg",
+    expected.insert(format!("{}{}", SIMPLE, "phone photo.jpg"),
                     dt(2016, 4, 16, 12, 27, 14));
 
-    let imgs = timexif::sequencer::get_images("tests/sample-imgs");
+    let imgs = timexif::sequencer::get_images(SIMPLE);
     for img in imgs {
         let path = img.path.to_str().unwrap();
         assert!(expected.contains_key(path));
